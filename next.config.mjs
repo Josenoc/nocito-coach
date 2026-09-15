e847b714b2a5b40d8582882e625a887298cea7db
Josenoc <josenoc@users.noreply.github.com>
2026-09-15 11:33:01 -0300

    Fix pdfkit en Vercel: externalizar y trazar fuentes estandar del serverless function

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['pdfkit'],
+  outputFileTracingIncludes: {
+    '/api/cron/check-payments': ['./node_modules/pdfkit/js/standard-fonts/**'],
+    '/api/webhooks/mercadopago': ['./node_modules/pdfkit/js/standard-fonts/**'],
+  },
 };
 
 export default nextConfig;
